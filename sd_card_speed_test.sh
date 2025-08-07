992348211242db1eeb7e84cd1d2aadf7a136f5e6
belamy <belamy@interia.pl>
2025-08-07 18:44:39 +0200

    Simplify speed test process.

--- sd_card_speed_test.sh
+++ sd_card_speed_test.sh
@@ -13,54 +13,18 @@ FILENAME="$SD_DIR/file_speed_test.deleteme"
 SIZE=1000 # in MB
 # SIZE=100 # in MB
 
-calc()
-{ 
-  awk "BEGIN { print "$*" }" | cut -d' ' -f1
+
+write_file() {
+  echo -n "Average write speed: "
+  dd if=/dev/zero of=$FILENAME bs=1M count=$SIZE oflag=direct 2>&1 | tail -n 1 | sed -E 's|^.* ([0-9.,]+ .B\/s)$|\1|'
 }
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  WRITE_FILE_COMMAND="mkfile ${SIZE}m $FILENAME"
-  CLEAR_CACHE_COMMAND="purge"
-else
-  WRITE_FILE_COMMAND="dd if=/dev/zero of=$FILENAME bs=1M count=$SIZE oflag=direct"
-  CLEAR_CACHE_COMMAND='sh -c echo 3 > /proc/sys/vm/drop_caches'
-fi
-READ_FILE_COMMAND="cat $FILENAME > /dev/null"
-
-RunTimeInSeconds() {
-  local TIMEFORMAT=%R
-  RES=`{ time $@ >/dev/null 2>&1; } 2>&1`
-  echo $RES
+read_file() {
+  echo -n "Average read speed: "
+  dd if=$FILENAME of=/dev/null bs=1M iflag=direct 2>&1 | tail -n 1 | sed -E 's|^.* ([0-9.,]+ .B\/s)$|\1|'
 }
 
-ClearFileCache() {
-  if sudo -n false 2>/dev/null; then
-    echo "Administrator password is required to clear file cache and get correct read test results."
-  fi
-  sudo $CLEAR_CACHE_COMMAND
-}
-
-WriteFile() {
-  echo "Writing ${SIZE}MB file $FILENAME..."
-  local WRITE_SECONDS=`RunTimeInSeconds $WRITE_FILE_COMMAND`
-  if [ -f "$FILENAME" ]; then
-    local WRITE_SPEED=$(calc $SIZE/$WRITE_SECONDS)
-    echo "Average sequential write speed: ${WRITE_SPEED}MB/sec."
-  else
-    echo "Error writing file $FILENAME"
-    false
-  fi
-}
-
-ReadFile() {
-  local READ_SECONDS=`RunTimeInSeconds $READ_FILE_COMMAND`
-  local READ_SPEED=$(calc $SIZE/$READ_SECONDS)
-  echo "Average sequential read speed: ${READ_SPEED}MB/sec."
-}
-
-WriteFile
-# Always delete created file on exit.
+write_file
 trap "rm $FILENAME" 0
-ClearFileCache
-ReadFile
+read_file
 
